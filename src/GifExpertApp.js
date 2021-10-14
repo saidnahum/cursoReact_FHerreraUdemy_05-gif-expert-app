2aec4eda3de17feba050bf0d4b76279c9caaeecf
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-10-14 01:45:55 -0500

    update

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 const GifExpertApp = () => {
+
+   const [ categories, setCategories ] = useState(['One Punch', 'Samurai X', 'Naruto']);
+
+   // const handleAdd = () => {
+   //    //setCategories([...categories, 'One Piece']);
+   //    setCategories(categories => [...categories, 'One Piece']);
+   // }
+
    return (
       <div>
          <h2>Gif Expert App</h2>
+         <AddCategory setCategories={ setCategories }/>
          <hr />
+
+         <ol>
+            {
+               categories.map((category, i) => {
+                  return <li key={i}>{category}</li>
+               })
+            }
+         </ol>
       </div>
    )
 }
